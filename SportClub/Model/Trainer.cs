7e08c506338614fd3cf9f52d005cd1aeb518e299
Spektrov Denys <denys.spektrov@nure.ua>
2021-11-26 20:02:30 +0200

    Add Trainer and TrainerConfig realisation.

--- SportClub/Model/Trainer.cs
+++ SportClub/Model/Trainer.cs
@@ -3,10 +3,165 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using SportClub.Annotations;
 
 namespace SportClub.Model
 {
-    class Trainer
+    class Trainer : INotifyPropertyChanged
     {
+        private int _trainerId;
+        private string _lastName;
+        private string _firstName;
+        private DateTime? _birthDate;
+        private string _gender;
+        private string _phoneNumber;
+        private string _email;
+        private DateTime _applyDate;
+        private string _education;
+        private string _sportAchivements;
+        private int? _experience;
+        private decimal? _salary;
+
+        public int TrainerId
+        {
+            get => _trainerId;
+            set
+            {
+                if (value == _trainerId) return;
+                _trainerId = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string LastName
+        {
+            get => _lastName;
+            set
+            {
+                if (value == _lastName) return;
+                _lastName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string FirstName
+        {
+            get => _firstName;
+            set
+            {
+                if (value == _firstName) return;
+                _firstName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public DateTime? BirthDate
+        {
+            get => _birthDate;
+            set
+            {
+                if (value.Equals(_birthDate)) return;
+                _birthDate = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Gender
+        {
+            get => _gender;
+            set
+            {
+                if (value == _gender) return;
+                _gender = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string PhoneNumber
+        {
+            get => _phoneNumber;
+            set
+            {
+                if (value == _phoneNumber) return;
+                _phoneNumber = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Email
+        {
+            get => _email;
+            set
+            {
+                if (value == _email) return;
+                _email = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public DateTime ApplyDate
+        {
+            get => _applyDate;
+            set
+            {
+                if (value.Equals(_applyDate)) return;
+                _applyDate = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Education
+        {
+            get => _education;
+            set
+            {
+                if (value == _education) return;
+                _education = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string SportAchivements
+        {
+            get => _sportAchivements;
+            set
+            {
+                if (value == _sportAchivements) return;
+                _sportAchivements = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int? Experience
+        {
+            get => _experience;
+            set
+            {
+                if (value == _experience) return;
+                _experience = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public decimal? Salary
+        {
+            get => _salary;
+            set
+            {
+                if (value == _salary) return;
+                _salary = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
--- SportClub/SportClubDbContext/TrainerConfig.cs
+++ SportClub/SportClubDbContext/TrainerConfig.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity.ModelConfiguration;
+using SportClub.Model;
 
-namespace SportClub.Model
+namespace SportClub.SportClubDbContext
 {
-    class Trainer
+    class TrainerConfig : EntityTypeConfiguration<Trainer>
     {
+        public TrainerConfig()
+        {
+            HasKey(trainer => trainer.TrainerId);
+            Property(trainer => trainer.LastName).IsRequired().HasMaxLength(50);
+            Property(trainer => trainer.FirstName).IsRequired().HasMaxLength(50);
+            Property(trainer => trainer.BirthDate).HasColumnType("datetime2").IsOptional();
+            Property(trainer => trainer.Gender).IsRequired().HasMaxLength(20);
+            Property(trainer => trainer.PhoneNumber).HasMaxLength(15).IsOptional();
+            Property(trainer => trainer.Email).HasMaxLength(30).IsOptional();
+            Property(trainer => trainer.ApplyDate).IsRequired();
+            Property(trainer => trainer.Education).IsOptional();
+            Property(trainer => trainer.SportAchivements).IsOptional();
+            Property(trainer => trainer.Experience).IsOptional();
+            Property(trainer => trainer.Salary).IsOptional();
+
+            ToTable("Trainers");
+        }
     }
 }
