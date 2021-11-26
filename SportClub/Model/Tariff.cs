3302016ceede7abe686589f55a5e82e66b3d36cf
Spektrov Denys <denys.spektrov@nure.ua>
2021-11-26 20:14:23 +0200

    Add Tariff and TariffConfig realisation.

--- SportClub/Model/Tariff.cs
+++ SportClub/Model/Tariff.cs
@@ -3,10 +3,94 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using SportClub.Annotations;
 
 namespace SportClub.Model
 {
-    class Tariff
+    class Tariff : INotifyPropertyChanged
     {
+        private int _tariffId;
+        private decimal _price;
+        private int _visitNumber;
+        private int _personalTraining;
+        private int _groupTraining;
+        private string _additionalInformation;
+
+        public int TariffId
+        {
+            get => _tariffId;
+            set
+            {
+                if (value == _tariffId) return;
+                _tariffId = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public decimal Price
+        {
+            get => _price;
+            set
+            {
+                if (value == _price) return;
+                _price = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int VisitNumber
+        {
+            get => _visitNumber;
+            set
+            {
+                if (value == _visitNumber) return;
+                _visitNumber = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int PersonalTraining
+        {
+            get => _personalTraining;
+            set
+            {
+                if (value == _personalTraining) return;
+                _personalTraining = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int GroupTraining
+        {
+            get => _groupTraining;
+            set
+            {
+                if (value == _groupTraining) return;
+                _groupTraining = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string AdditionalInformation
+        {
+            get => _additionalInformation;
+            set
+            {
+                if (value == _additionalInformation) return;
+                _additionalInformation = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
--- SportClub/SportClubDbContext/TariffConfig.cs
+++ SportClub/SportClubDbContext/TariffConfig.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity.ModelConfiguration;
+using SportClub.Model;
 
-namespace SportClub.Model
+namespace SportClub.SportClubDbContext
 {
-    class Tariff
+    class TariffConfig : EntityTypeConfiguration<Tariff>
     {
+        public TariffConfig()
+        {
+            HasKey(tariff => tariff.TariffId);
+            Property(tariff => tariff.Price).IsRequired();
+            Property(tariff => tariff.VisitNumber).IsRequired();
+            Property(tariff => tariff.PersonalTraining).IsRequired();
+            Property(tariff => tariff.GroupTraining).IsRequired();
+            Property(tariff => tariff.AdditionalInformation).IsOptional();
+        }
     }
 }
