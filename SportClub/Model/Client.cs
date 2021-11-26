67de6022b2f3ff1479b8445d7da936a8b3d2caf1
Spektrov Denys <denys.spektrov@nure.ua>
2021-11-26 19:35:21 +0200

    Add Client and ClientConfig realisation.

--- SportClub/Model/Client.cs
+++ SportClub/Model/Client.cs
@@ -3,10 +3,118 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using SportClub.Annotations;
+
 
 namespace SportClub.Model
 {
-    class Client
+    class Client : INotifyPropertyChanged
     {
+        private int _clientId;
+        private string _lastName;
+        private string _firstName;
+        private DateTime? _birthDate;
+        private string _gender;
+        private string _phoneNumber;
+        private string _email;
+        private DateTime _registrationDate;
+
+        public int ClientId
+        {
+            get => _clientId;
+            set
+            {
+                if (value == _clientId) return;
+                _clientId = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string LastName
+        {
+            get => _lastName;
+            set
+            {
+                if (value == _lastName) return;
+                _lastName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string FirstName
+        {
+            get => _firstName;
+            set
+            {
+                if (value == _firstName) return;
+                _firstName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public DateTime? BirthDate
+        {
+            get => _birthDate;
+            set
+            {
+                if (value.Equals(_birthDate)) return;
+                _birthDate = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Gender
+        {
+            get => _gender;
+            set
+            {
+                if (value == _gender) return;
+                _gender = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string PhoneNumber
+        {
+            get => _phoneNumber;
+            set
+            {
+                if (value == _phoneNumber) return;
+                _phoneNumber = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Email
+        {
+            get => _email;
+            set
+            {
+                if (value == _email) return;
+                _email = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public DateTime RegistrationDate
+        {
+            get => _registrationDate;
+            set
+            {
+                if (value.Equals(_registrationDate)) return;
+                _registrationDate = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
--- SportClub/SportClubDbContext/ClientConfig.cs
+++ SportClub/SportClubDbContext/ClientConfig.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity.ModelConfiguration;
+using SportClub.Model;
 
-namespace SportClub.Model
+namespace SportClub.SportClubDbContext
 {
-    class Client
+    class ClientConfig : EntityTypeConfiguration<Client>
     {
+        public ClientConfig()
+        {
+            HasKey(client => client.ClientId);
+            Property(client => client.LastName).IsRequired().HasMaxLength(50);
+            Property(client => client.FirstName).IsRequired().HasMaxLength(50);
+            Property(client => client.BirthDate).HasColumnType("datetime2").IsOptional();
+            Property(client => client.Gender).IsRequired().HasMaxLength(20);
+            Property(client => client.PhoneNumber).HasMaxLength(15).IsOptional();
+            Property(client => client.Email).HasMaxLength(30).IsOptional();
+            Property(client => client.RegistrationDate).IsRequired();
+
+            ToTable("Clients");
+        }
     }
 }
